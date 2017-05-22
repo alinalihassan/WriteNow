badd7e0e87023f136a951bead29a15ed0014ca32
Hassan Alin Ali <superalin40@gmail.com>
2017-05-22 15:39:50 +0300

    Disabled Dev Tools

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ function createWindow () {
     // and load the index.html of the app.
     mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-    mainWindow.toggleDevTools();
+    // mainWindow.toggleDevTools();
     mainWindow.on('closed', function() {
         mainWindow = null;
     });
